85bc65e09fe2ea6ab2b13a3425007693cb663cea
Saahil Ali <programmer290399@gmail.com>
2018-10-01 10:04:22 +0530

    Delete scrape_images.py

--- scrape_images.py
+++
@@ -1,7 +0,0 @@
-from google_images_download import google_images_download   
-
-response = google_images_download.googleimagesdownload()   
-item = input("enter the keywords :")
-arguments = {"keywords": item ,"limit":1 ,"print_urls":True}  
-paths = response.download(arguments)   
-print(paths)   
