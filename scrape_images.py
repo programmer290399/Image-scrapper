26d96b6602d422ac93588e338b0e5bb4ef392611
programmer290399 <programmer290399>
2018-12-02 16:54:25 +0530

    new version

--- scrape_images.py
+++ scrape_images.py
@@ -1,7 +1,20 @@
-from google_images_download import google_images_download   
+from gid import gid
+from google_images_download import google_images_download 
 
-response = google_images_download.googleimagesdownload()   
-item = input("enter the keywords :")
-arguments = {"keywords": item ,"limit":1 ,"print_urls":True}  
-paths = response.download(arguments)   
-print(paths)   
+response = gid.googleimagesdownload()
+typ = input('Enter type (d/nd):')
+if typ == 'd' :
+
+
+    response = google_images_download.googleimagesdownload()   
+    item = input("enter the keywords :")
+    arguments = {"keywords": item ,"limit":1 ,"print_urls":True}  
+    paths = response.download(arguments)   
+    print(paths) 
+
+else :
+
+    item = input("enter the keywords :")
+    arguments = {"keywords": item ,"limit":2 ,"print_urls":False , 'no_download': True} 
+    image_paths = response.download(arguments)
+    print(image_paths)
